045ff12bb43ed4631f2a4751dfaff87718a70c6d
Anderson C <anderson.zaratustra@gmail.com>
2019-09-07 13:19:02 -0300

    Criando domain Categoria

--- src/main/java/com/anderson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/anderson/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,29 @@
 package com.anderson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.anderson.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar(){
+	public List<Categoria> listar(){
 		
-		return "Listar Categoria";
+		Categoria categoria1 = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Informatica");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 		
 	}
 
