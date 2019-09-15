c8314d4354c77afbfe7cbfe441856ca7a5769757
Anderson C <anderson.zaratustra@gmail.com>
2019-09-15 19:57:52 -0300

    validation categoria

--- src/main/java/com/anderson/cursomc/services/CategoriaService.java
+++ src/main/java/com/anderson/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.anderson.cursomc.domain.Categoria;
+import com.anderson.cursomc.dto.CategoriaDTO;
 import com.anderson.cursomc.repositories.CategoriaRepository;
 import com.anderson.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -48,6 +49,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),	orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
