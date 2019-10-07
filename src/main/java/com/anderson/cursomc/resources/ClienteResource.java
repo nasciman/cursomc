95aa8a41f02026a49da2078c6a7aca4bb1144617
Anderson C <anderson.zaratustra@gmail.com>
2019-10-06 21:03:15 -0300

    PUT, GET, DELETE para cliente

--- src/main/java/com/anderson/cursomc/resources/ClienteResource.java
+++ src/main/java/com/anderson/cursomc/resources/ClienteResource.java
@@ -1,14 +1,25 @@
 package com.anderson.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anderson.cursomc.domain.Cliente;
+import com.anderson.cursomc.dto.ClienteDTO;
 import com.anderson.cursomc.services.ClienteService;
+import com.anderson.cursomc.services.exceptions.DataIntegrityException;
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -18,12 +29,53 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Cliente> listar(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok(obj);
 		
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(categoria -> new ClienteDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0" )Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "25" )Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "nome" )String orderBy, 
+			@RequestParam(value="direction", defaultValue = "ASC" )String direction){
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(categoria -> new ClienteDTO(categoria));
+		return ResponseEntity.ok(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
 
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		
+		try{
+			service.delete(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+		return ResponseEntity.noContent().build();
+	}
+	
 }
